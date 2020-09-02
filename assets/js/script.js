5dd3cc5b77f24292d791da044e720af942fd8d18
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-02 14:27:36 -0500

    added drag and drop feature to update/delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,6 +135,66 @@ $(".list-group").on("click", "p", function() {
     $(this).replaceWith(taskSpan);
   });
 
+  $(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+    },
+    deactivate: function(event) {
+    },
+    over: function(event) {
+    },
+    out: function(event) {
+    },
+    update: function(event) {
+
+      // array to store the task data in 
+      var tempArr = [];
+
+      // loop over current set of children in sortable list
+      $(this).children().each(function() {
+        var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+        var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+        // add task data to the temp array as an object
+        tempArr.push ({
+          text: text, 
+          date: date
+        });
+      });
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+    }
+  });
+
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      console.log("drop");
+      ui.draggable.remove();
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
+
   
 
 
